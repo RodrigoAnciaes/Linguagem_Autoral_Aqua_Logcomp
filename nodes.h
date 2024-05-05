3a73665f296a1213c40b6262fe0f3524d47ae1f3
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2024-05-05 17:03:08 -0300

    Adding my nodes.

--- nodes.h
+++ nodes.h
@@ -11,6 +11,30 @@ typedef std::vector<NStatement*> StatementList;
 typedef std::vector<NExpression*> ExpressionList;
 typedef std::vector<NVariableDeclaration*> VariableList;
 
+/*
+EBNF:
+
+BLOCK = { STATEMENT };
+STATEMENT = ( "λ" | SPAW | DISCOVER | SUSTAIN | EVENT | RAIN | DRY | EXTINCTION | OPERATION ), "\n" ;
+SPAW = TYPE, IDENTIFIER, NUMBER, ( "λ" | NUMBER ) ;
+DISCOVER = "discover", "(", IDENTIFIER, ")" ;
+SUSTAIN = IDENTIFIER, "sustains", IDENTIFIER, "\n", "λ", { ( STATEMENT ), "λ" }, "pass_time" ;
+EVENT = "event", IDENTIFIER, COMPARISSON, IDENTIFIER, "\n", "λ", { ( STATEMENT ), "λ" }, "conclude" ;
+RAIN = "rain","(", IDENTIFIER, ")" ;
+DRY = "dry","(", IDENTIFIER, ")" ;
+EXTINCTION = "extingish", IDENTIFIER ; 
+OPERATION = IDENTIFIER, OP_T, NUMBER, ( "λ" | OP_T, IDENTIFIER ) ;
+IDENTIFIER = LETTER, { LETTER | DIGIT | "_" } ;
+NUMBER = DIGIT, { DIGIT } ;
+LETTER = ( "a" | "..." | "z" | "A" | "..." | "Z" ) ;
+DIGIT = ( "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0" );
+TYPE = ( "river" | "fish" ) ;
+OP_T = ( "branch" | "acumulate" | ">>" | "->" ) ;
+
+*/
+
+/* Template Nodes */
+
 class Node {
 public:
     virtual ~Node() {}
@@ -111,3 +135,106 @@ public:
         type(type), id(id), arguments(arguments), block(block) { }
     virtual llvm::Value* codeGen(CodeGenContext& context);
 };
+
+/* My nodes*/
+
+class NSpawn : public NStatement {
+public:
+    const NIdentifier& type;
+    NIdentifier& id;
+    NInteger& number;
+    NInteger& number2;
+    NSpawn(const NIdentifier& type, NIdentifier& id, NInteger& number, NInteger& number2) :
+        type(type), id(id), number(number), number2(number2) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NDiscover : public NStatement {
+public:
+    NIdentifier& id;
+    NDiscover(NIdentifier& id) : id(id) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NSustain : public NStatement {
+public:
+    NIdentifier& id;
+    NIdentifier& id2;
+    StatementList statements;
+    NSustain(NIdentifier& id, NIdentifier& id2, StatementList& statements) :
+        id(id), id2(id2), statements(statements) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NEvent : public NStatement {
+public:
+    NIdentifier& id;
+    int comp;
+    NIdentifier& id2;
+    StatementList statements;
+    NEvent(NIdentifier& id, int comp, NIdentifier& id2, StatementList& statements) :
+        id(id), comp(comp), id2(id2), statements(statements) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NRain : public NStatement {
+public:
+    NIdentifier& id;
+    NRain(NIdentifier& id) : id(id) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NDry : public NStatement {
+public:
+    NIdentifier& id;
+    NDry(NIdentifier& id) : id(id) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NExtinction : public NStatement {
+public:
+    NIdentifier& id;
+    NExtinction(NIdentifier& id) : id(id) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NOperation : public NStatement {
+public:
+    NIdentifier& id;
+    int op;
+    NInteger& number;
+    NIdentifier& id2;
+    NOperation(NIdentifier& id, int op, NInteger& number, NIdentifier& id2) :
+        id(id), op(op), number(number), id2(id2) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+
+class NIdentifier : public NExpression {
+public:
+    std::string name;
+    NIdentifier(const std::string& name) : name(name) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NNumber : public NExpression {
+public:
+    long long value;
+    NNumber(long long value) : value(value) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NType : public NExpression {
+public:
+    std::string name;
+    NType(const std::string& name) : name(name) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
+class NOperationType : public NExpression {
+public:
+    std::string name;
+    NOperationType(const std::string& name) : name(name) { }
+    virtual llvm::Value* codeGen(CodeGenContext& context);
+};
+
